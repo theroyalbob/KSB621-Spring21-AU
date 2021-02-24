b860f9e76a8c3d98688c67f81645f774d22ffd43
theroyalbob <a.stew1998@gmail.com>
2021-02-24 13:52:16 -0500

    Create ImportData.R

---
+++ RCode/ImportData.R
@@ -0,0 +1,4 @@
+base <- read.csv("C:/Users/as371/Desktop/Spring 2021/KSB621-Spring21-AU/InventoryData/part_base.csv")
+location <- read.csv("C:/Users/as371/Desktop/Spring 2021/KSB621-Spring21-AU/InventoryData/part_location.csv")
+log <- read.csv("C:/Users/as371/Desktop/Spring 2021/KSB621-Spring21-AU/InventoryData/part_log.csv")
+transaction <- read.csv("C:/Users/as371/Desktop/Spring 2021/KSB621-Spring21-AU/InventoryData/part_transaction.csv")
