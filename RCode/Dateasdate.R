e55c973c7b829c1a96907af2052788eeb212e3b7
theroyalbob <a.stew1998@gmail.com>
2021-03-02 19:39:33 -0500

    Update Dateasdate.R
    Changed all date classes

---
+++ RCode/Dateasdate.R
@@ -0,0 +1,4 @@
+log$DateChanged<- as.Date(log$DateChanged)
+transaction$TransactionDate <- as.Date(transaction$TransactionDate)
+base$CreatedOn <- as.Date(base$CreatedOn)
+base$LastModifiedOn<- as.Date(base$LastModifiedOn)
